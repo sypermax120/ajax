df6cd9025af91e2bbe29c2c163e1d9e5007cde60
sypermax120 <49924496+sypermax120@users.noreply.github.com>
2020-05-23 14:08:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
 const express = require('express');
 const app = express();
 
-app.listen(3000)
 
 app.use(express.static('public'));
 
@@ -12,13 +10,24 @@ app.get('/ajax', function(req, res){
 })
 
 app.get("/resp", function (request, response) {
-    var login = response.login;
+    var login = request.log;
     var pass = response.pass;
+    response.set('Access-Control-Allow-Origin', '*');
+    response.set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    // response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    // response.statusCode = 302;
+    console.log("Url: " + request.url);
+    console.log(request.headers);
+    console.log(`${login} - ${pass}`);
     response.send(`${login} - ${pass}`);
+    response.end();
 });
 
 app.get('/asd1', function(req, res){
     console.log("ok");
     var dat = new Date();
     res.end(dat.toString());
-})
+});
+
+app.listen(3000);
