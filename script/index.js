4e732cd1bf7a1cc1d0db0d188ffe80f1169ffe6d
sypermax120 <49924496+sypermax120@users.noreply.github.com>
2020-05-23 14:09:36 +0300

    Update index.js

--- script/index.js
+++ script/index.js
@@ -1,12 +1,40 @@
 document.getElementsByClassName("buttomn1")[0].addEventListener('click', function(){
-    var login = document.getElementsByClassName('pole1')[0].value;
+    var log = document.getElementsByClassName('pole1')[0].value;
     var pass = document.getElementsByClassName('pole2')[0].value;
 
-    if (login == "maks" && pass == "123"){
-        console.log(document.getElementsByClassName('pole1')[0].value);
-        console.log(document.getElementsByClassName('pole2')[0].value);
-        window.location.href = "page2.html";
+    // if (login == "maks" && pass == "123"){
+    //     console.log(document.getElementsByClassName('pole1')[0].value);
+    //     console.log(document.getElementsByClassName('pole2')[0].value);
+    //     window.location.href = "page2.html";
 
-    }
+    // }
+    
+    var method = "GET"
+    var URL = 'http://localhost:3000/resp';
+    var async = true;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open(method, URL, async)
+    xhr.send({"log": log , "pass": pass});
+    
+
+    // 4. Этот код сработает после того, как мы получим ответ сервера
+    xhr.onload = function() {
+        if (xhr.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`); // Например, 404: Not Found
+        } else { // если всё прошло гладко, выводим результат
+            console.log(`Готово`); // response -- это ответ сервера
+        }
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+
+        if(xhr.status == 200){
+            window.location.href = "page2.html";
+            
+        };
+    };
+
+    console.log('ok');
 
 });
